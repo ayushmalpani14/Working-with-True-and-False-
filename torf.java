d7a3d5e3f328ccafbcc5f06e777a812a3247121f
ayushmalpani14 <ayushmalpani014@gmail.com>
2026-06-19 20:28:50 +0530

    these are my files and screenshots .

---
+++ torf.java
@@ -0,0 +1,18 @@
+class hello
+
+ { public static void main(String arg[]) 
+     {
+
+     int x = 6; 
+     int y = 9; 
+    
+    int a = 4; 
+    int b = 3; 
+
+     boolean result = x < y && a < b ;
+
+     System.out.println(!result);
+
+
+ }
+}
